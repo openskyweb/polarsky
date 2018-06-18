3de6fb14c8226f268e094b2b07141df1645c3658
Karena Kreger <karenalenore@gmail.com>
2018-06-18 12:20:38 -0400

    Add Full width Images

--- functions.php
+++ functions.php
@@ -78,5 +78,24 @@ function opensky_enqueue_scripts_styles() {
 	wp_enqueue_style( 'custom', get_stylesheet_directory_uri() . '/custom.css', array( 'generate-child' ) );
 }
 
+// Image size for full width backgrounds
+add_image_size( 'full-width', 2048, 2048, false ); // width, height, crop
+
+// Add Additional Image Sizes to Beaver Builder chooser
+// https://kb.wpbeaverbuilder.com/article/382-add-custom-image-sizes
+add_filter('image_size_names_choose', 'insert_custom_image_sizes'); 
+function insert_custom_image_sizes($sizes) {
+	global $_wp_additional_image_sizes;
+	if (empty($_wp_additional_image_sizes)) {
+		return $sizes;
+	}
+	foreach ($_wp_additional_image_sizes as $id => $data) {
+		if (!isset($sizes[$id])) {
+			$sizes[$id] = ucfirst(str_replace('-', ' ', $id));
+		}
+	}
+	return $sizes;
+}
+
 //* Include More PHP Customizations
 file_exists(get_stylesheet_directory() . '/custom.php') AND include_once get_stylesheet_directory() . '/custom.php';
