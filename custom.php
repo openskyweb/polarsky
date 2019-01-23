1de96287cd8a2b9c918858cd4c2a277a9faaf106
Karena Kreger <karenalenore@gmail.com>
2019-01-23 09:23:07 -0500

    Add code for Custom Fonts

--- custom.php
+++ custom.php
@@ -1,3 +1,19 @@
 <?php
 /* Add Your Custom PHP Below This Line
 ---------------------------------------------------------------- */
+
+// Enqueue Fonts
+//add_action( 'wp_enqueue_scripts', 'opensky_enqueue_scripts_fonts' );
+function opensky_enqueue_scripts_fonts() {
+	wp_enqueue_style( 'opensky-fonts', '//fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i', array(), CHILD_THEME_VERSION );
+}
+// Add a new font to GeneratePress https://docs.generatepress.com/article/customizing-the-google-font-list/
+//add_filter( 'generate_typography_customize_list', 'opensky_add_google_fonts' );
+function opensky_add_google_fonts( $fonts ) {
+	$fonts[ 'montserrat' ] = array( 
+		'name' => 'Montserrat',
+		'variants' => array( '400', '400i', '700', '700i' ),
+		'category' => 'sans-serif'
+	);
+	return $fonts;
+}
