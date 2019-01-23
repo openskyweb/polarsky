a2fcbbe99693d390fcdca0320f0a218745ff9feb
Karena Kreger <karenalenore@gmail.com>
2019-01-23 08:42:20 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -40,13 +40,13 @@ function opensky_copyright_remove_default_message() {
 //add_action( 'generate_credits','opensky_credits_function' );
 add_shortcode('credits', 'opensky_credits_function');
 function opensky_credits_function( $atts, $content = null ) {
-   extract( shortcode_atts( array(
-      'oscredit' => 'true', //set true|false for link to Open Sky
-      'title' => 'Open Sky Web Studio',
-      'link' => 'http://www.openskywebstudio.com',
-      'link_title' => 'Open Sky Web Studio | Clean, Effective Websites',
-      'linebreak' => 'true',
-      ), $atts ) );
+	extract( shortcode_atts( array(
+		'oscredit' => 'true', //set true|false for link to Open Sky
+		'title' => 'Open Sky Web Studio',
+		'link' => 'http://www.openskywebstudio.com',
+		'link_title' => 'Open Sky Web Studio | Clean, Effective Websites',
+		'linebreak' => 'true',
+	), $atts ) );
 	$creds = "<span id='credits'>Copyright &copy; " . date('Y') . " <a href='".get_option('home')."' title='".get_bloginfo('name')." | ".get_bloginfo('description')."'>".get_bloginfo('name')."</a>. All Rights Reserved. ";
 	if (strtolower($oscredit) == "false")
 		$creds .= "<!--";
