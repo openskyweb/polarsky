dea8065ffd3aec69a463df6541dafd1012722a15
Karena Kreger <karenalenore@gmail.com>
2019-06-21 15:40:22 -0400

    Update custom.php

--- custom.php
+++ custom.php
@@ -9,19 +9,28 @@ function opensky_enqueue_scripts_fonts() {
 }
 // Add a new GOOGLE font to GeneratePress 
 // https://docs.generatepress.com/article/customizing-the-google-font-list/
-//add_filter( 'generate_typography_customize_list', 'opensky_add_google_fonts' );
-function opensky_add_google_fonts( $fonts ) {
-	$fonts[ 'montserrat' ] = array( 
-		'name' => 'Montserrat',
-		'variants' => array( '400', '400i', '700', '700i' ),
-		'category' => 'sans-serif'
+//add_filter( 'generate_typography_customize_list', 'opensky_add_google_fonts_gp' );
+function opensky_add_google_fonts_gp( $fonts ) {
+	$fonts[ 'marydale' ] = array( 
+		'name' => '',
+		'variants' => array( '400', '700' ),
+		'category' => 'serif'
 	);
 	return $fonts;
 }
 // Add a new LOCAL font to the GeneratePress
 // https://docs.generatepress.com/article/adding-local-fonts/
-//add_filter( 'generate_typography_default_fonts', 'opensky_add_local_fonts');
-function opensky_add_local_fonts( $fonts ) {
+//add_filter( 'generate_typography_default_fonts', 'opensky_add_local_fonts_gp');
+function opensky_add_local_fonts_gp( $fonts ) {
 	$fonts[] = 'Marydale';
 	return $fonts;
+}
+// Add a new LOCAL font to Beaver Builder Page Builder modules
+//add_filter( 'fl_builder_font_families_system', 'opensky_add_local_fonts_bb' );
+function opensky_add_local_fonts_bb ( $system_fonts ) {
+	$system_fonts[ 'Marydale' ] = array(
+		'fallback' => 'Times, serif',
+		'weights' => array(	'400' ),
+	);
+	return $system_fonts;
 }
