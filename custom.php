4faad55a0c0116bc227d60db04dfc9b809136067
Karena Kreger <karenalenore@gmail.com>
2019-06-21 14:53:44 -0400

    Update custom.php

--- custom.php
+++ custom.php
@@ -7,7 +7,8 @@
 function opensky_enqueue_scripts_fonts() {
 	wp_enqueue_style( 'opensky-fonts', '//fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i', array(), CHILD_THEME_VERSION );
 }
-// Add a new font to GeneratePress https://docs.generatepress.com/article/customizing-the-google-font-list/
+// Add a new GOOGLE font to GeneratePress 
+// https://docs.generatepress.com/article/customizing-the-google-font-list/
 //add_filter( 'generate_typography_customize_list', 'opensky_add_google_fonts' );
 function opensky_add_google_fonts( $fonts ) {
 	$fonts[ 'montserrat' ] = array( 
@@ -15,5 +16,12 @@ function opensky_add_google_fonts( $fonts ) {
 		'variants' => array( '400', '400i', '700', '700i' ),
 		'category' => 'sans-serif'
 	);
+	return $fonts;
+}
+// Add a new LOCAL font to the GeneratePress
+// https://docs.generatepress.com/article/adding-local-fonts/
+//add_filter( 'generate_typography_default_fonts', 'opensky_add_local_fonts');
+function opensky_add_local_fonts( $fonts ) {
+	$fonts[] = 'Marydale';
 	return $fonts;
 }
