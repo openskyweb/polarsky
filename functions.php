698e3259e97466b8e97fc77fde150bcf5e037717
Karena Kreger <karenalenore@gmail.com>
2021-03-05 10:10:57 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -79,22 +79,17 @@ function opensky_enqueue_scripts_styles() {
 }
 
 // Image size for full width backgrounds
-//add_image_size( 'full-width', 2048, 2048, false ); // width, height, crop
-
-// Add Additional Image Sizes to Beaver Builder chooser
-// https://kb.wpbeaverbuilder.com/article/382-add-custom-image-sizes
-add_filter('image_size_names_choose', 'insert_custom_image_sizes'); 
-function insert_custom_image_sizes($sizes) {
-	global $_wp_additional_image_sizes;
-	if (empty($_wp_additional_image_sizes)) {
-		return $sizes;
-	}
-	foreach ($_wp_additional_image_sizes as $id => $data) {
-		if (!isset($sizes[$id])) {
-			$sizes[$id] = ucfirst(str_replace('-', ' ', $id));
-		}
-	}
-	return $sizes;
+// https://wordpress.stackexchange.com/questions/103995/add-image-size-and-add-filterimage-size-names-choose-my-custom-image-sizes
+add_action( 'after_setup_theme', 'opensky_add_image_sizes' );
+function opensky_add_image_sizes() {
+	add_image_size( 'container-width', 1400, 1400, false ); // width, height, crop
+}
+add_filter('image_size_names_choose', 'opensky_add_sizes_to_chooser');
+function opensky_add_sizes_to_chooser($default_sizes) {
+	$new_sizes = array (
+		'container-width' => 'Container Width'
+	);
+	return array_merge($default_sizes, $new_sizes);
 }
 
 // Disable Full Screen as default editing in Block Editor
